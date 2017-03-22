aad7b724d3ccc2ceac00477bf907e80bc383271e
stagiaire <stagiaire>
2017-03-22 16:55:59 +0100

    ajout avatar associé à la création de l'objet joueur

--- public/js/player.js
+++ public/js/player.js
@@ -1,10 +1,11 @@
 
 Player = function Player(conf) {
 	"use strict";
+	var avatar = select("#avatars .active img");
 	this.pseudo = conf.pseudo || "random player";
 	this.points = 0;
 	this.cartes = [];
-	this.avatar = null;
+	this.avatar = avatar ?  avatar.src : "medias/images/avatars/n05.png";
 };
 
 Player.prototype.poserCartes = function poserCartes(modificateur) {
