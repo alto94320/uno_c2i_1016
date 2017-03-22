d94b6da99f3ecfbada6686319bfe6b9c6fa78544
alto94320 <b.kilman@outlook.fr>
2017-03-22 12:02:24 +0100

    Add files via upload
    socket_io

---
+++ socket_io/app.js
@@ -0,0 +1,154 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+GNU nano 2.2.6                                                          Fichier : app.js                                                                                                                         
+
+// Call the console.log function.
+console.log("Hello World");
+
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
+
+// Load the net module to create a tcp server.
+var net = require('net');
+
+// Creates a new TCP server. The handler argument is automatically set as a listener for the 'connection' event
+var server = net.createServer(function (socket) {
+
+  // Every time someone connects, tell them hello and then close the connection.
+  console.log("Connection from " + socket.remoteAddress);
+  socket.end("Hello World\n");
+
+});
+
+// Fire up the server bound to port 7000 on localhost
+server.listen(7000, "localhost");
+
+// Put a friendly message on the terminal
+console.log("TCP server listening on port 7000 at localhost.");
+
+io.sockets.on('connection', function (socket) {
+
+        socket.emit('message', 'Vous êtes bien connecté !');
+
+});
+
+app.listen(80);
+
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+var app = require('express').createServer();
+var io = require('socket.io')(app);
+
+app.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+// note, io(<port>) will create a http server for you
+var io = require('socket.io')(80);
+
+io.on('connection', function (socket) {
+  io.emit('this', { will: 'be received by everyone'});
+
+  socket.on('private message', function (from, msg) {
+    console.log('I received a private message by ', from, ' saying ', msg);
+  });
+
+  socket.on('disconnect', function () {
+    io.emit('user disconnected');
+  });
+});
+
+var io = require('socket.io')(80);
+var chat = io
+  .of('/chat')
+  .on('connection', function (socket) {
+    socket.emit('a message', {
+        that: 'only'
+      , '/chat': 'will get'
+    });
+    chat.emit('a message', {
+        everyone: 'in'
+      , '/chat': 'will get'
+    });
+  });
+
+var news = io
+  .of('/news')
+  .on('connection', function (socket) {
+    socket.emit('item', { news: 'item' });
+  });
+
+
+var io = require('socket.io')(80);
+
+io.on('connection', function (socket) {
+  var tweets = setInterval(function () {
+    getBieberTweet(function (tweet) {
+      socket.volatile.emit('bieber tweet', tweet);
+    });
+  }, 100);
+
+  socket.on('disconnect', function () {
+    clearInterval(tweets);
+  });
+});
+
+var io = require('socket.io')(80);
+
+io.on('connection', function (socket) {
+  socket.on('ferret', function (name, fn) {
+    fn('woot');
+  });
+});
+
+
+var io = require('socket.io')(80);
+
+io.on('connection', function (socket) {
+  socket.broadcast.emit('user connected');
+});
+
+var io = require('socket.io')(80);
+
+io.on('connection', function (socket) {
+  socket.on('message', function () { });
+  socket.on('disconnect', function () { });
+});
